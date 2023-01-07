405f838ff0a33015b1204b031bae05e96964c80c
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-01-07 12:44:35 +0200

    added analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import{
+import {
   BrowserRouter,
   Routes,
   Route,
@@ -6,15 +6,17 @@ import{
 import Tickets from "./pages/shop/Tickets";
 import Reservation from "./pages/reservation/Reservation"
 import Payment from "./pages/Payments/Payment"
+import Analytics from "./pages/Analytics";
 
 function App() {
   return (
     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Tickets/>}/>
-      <Route path="/Reservation" element={<Reservation/>}/>
-      <Route path="/Payment" element={<Payment/>}/>
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Tickets />} />
+        <Route path="/Reservation" element={<Reservation />} />
+        <Route path="/Payment" element={<Payment />} />
+        <Route path="/analytics" element={<Analytics />} />
+      </Routes>
     </BrowserRouter>
   );
 }
